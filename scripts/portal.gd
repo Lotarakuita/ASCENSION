426f14cd005bae44588345bb926eab93ea0957e1
Lotarakuita <lotsau96@kingshigh.school.nz>
2025-06-25 11:55:34 +1200

    Worked on buttons

--- scripts/portal.gd
+++ scripts/portal.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Pdlayer":
-		get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://scenes/Win screen 1.tscn")
