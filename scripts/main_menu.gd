615834171c7257244cf8c96751c257d9f9c7a055
Lotarakuita <lotsau96@kingshigh.school.nz>
2025-07-17 12:22:45 +1200

    Worked on the Settings Menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_(test).tscn")
 
 func _on_settings_pressed() -> void:
-	get
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
