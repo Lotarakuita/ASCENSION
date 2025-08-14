fe891eff93c7cc0be1c5b4c256f3a95b70125b73
Lotarakuita <lotsau96@kingshigh.school.nz>
2025-08-14 14:58:48 +1200

    Null

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var main_buttons: VBoxContainer = $"Main Buttons"
 @onready var options: Panel = $Options
-
+@onready var control_panel : Panel = $"Control Panel"
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_(test).tscn")
@@ -21,3 +21,9 @@ func _on_exit_pressed() -> void:
 
 func _on_back_options_pressed() -> void:
 	_ready()
+
+
+func _on_controls_pressed() -> void:
+	main_buttons.visible = false
+	options.visible = false 
+	control_panel.visible = true
